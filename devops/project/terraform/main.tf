e4c85da8319a24218cf6d3100ed70c09d6294811
ashjd1 <ashutoshjadhav1122000@gmail.com>
2025-09-14 12:49:56 +0530

    ip to id

--- devops/project/terraform/main.tf
+++ devops/project/terraform/main.tf
@@ -6,4 +6,10 @@ resource "aws_instance" "dev" {
     instance_type   = var.ashu_type
     key_name        = var.ashu_key
     subnet_id       = var.ashu_subnet
+  # 👇 Attach existing SG
+  vpc_security_group_ids = ["sg-0f420010e39e12c23"]
+
+  tags = {
+    Name = var.ashu_name
+  }
 }
